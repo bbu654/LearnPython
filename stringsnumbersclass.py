dfb2c7d680e826ffe932070c3ab639c6129b750a
bbu654 <bbu654@hotmail.com>
2020-11-10 19:33:59 -0800

    Delete stringsnumbersclass.py

--- stringsnumbersclass.py
+++
@@ -1,9 +0,0 @@
-class Patient:
-    def __init__(self, name, age, is_new):
-        self.name = name
-        self.age = age
-        self.is_new = is_new
-
-    def printpatient(pat):
-        print("{} is ('' if {} else 'not ')a new patient. he is {}.".format(name,is_new,age))
-    
