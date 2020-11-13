4e0dcad8e244cf0af612003f6cf1e988d0360283
bbu654 <bbu654@hotmail.com>
2020-11-13 14:19:03 -0800

    Add files via upload

---
+++ LearningPy/bbutils.py
@@ -0,0 +1,12 @@
+def printit():
+    print("String usage:             \t\t'...' in Strng - => bool  ")
+    print("len(String)    - CharCount\t\tString.find()  - => index \t\tString.replace() -   ")
+    print("String.upper() - UPPERCASE\t\tString.lower() - lowercase\t\tString.title()  - CamelCase  ")
+    print("    ")
+    print("    ")
+    print("Arithmetic operators")
+    print("10//3          => int     \t\tx += 3,+,-,*,             \t\t/ => floating point")
+    print("operator precedance first to last: (),**,/,*,//,%,+,- \t\t\t\tround() abs() import math math.ceil")          
+    #print("hello from bbutils")
+
+
