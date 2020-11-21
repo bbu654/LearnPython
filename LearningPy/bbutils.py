4196a07cf9f12ef82df6c213f015da3437c532c2
bbu654 <bbu654@hotmail.com>
2020-11-20 18:10:44 -0800

    Add files via upload

--- LearningPy/bbutils.py
+++ LearningPy/bbutils.py
@@ -419,4 +419,37 @@ class whileforloops:
 #    shorty.append(whiley.userange(4,3))
 
 
+# Python program to print 
+# colored text and background 
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+
+#prCyan("Hello World, ") 
+#prYellow("It's") 
+#prGreen("Geeks") 
+#prRed("For") 
+#prGreen("Geeks") 
+# Python program to print 
+# colored text and background 
+def print_format_table(): 
+	""" 
+	prints table of formatted text format options 
+	"""
+	for style in range(8): 
+		for fg in range(30, 38): 
+			s1 = '' 
+			for bg in range(40, 48): 
+				format = ';'.join([str(style), str(fg), str(bg)]) 
+				s1 += '\x1b[%sm %s \x1b[0m' % (format, format) 
+			print(s1) 
+		print('\n') 
+
+#print_format_table() 
+
  
