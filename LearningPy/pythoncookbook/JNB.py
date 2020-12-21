580dac1d814eff018df021e6bd852e42dcc62143
bbu654 <bbu654@hotmail.com>
2020-12-20 20:59:49 -0800

    Add files via upload

--- LearningPy/pythoncookbook/JNB.py
+++ LearningPy/pythoncookbook/JNB.py
@@ -1,7 +1,4 @@
-# example.py
-#
-# Remove duplicate entries from a sequence while keeping order
-
+# %%
 def dedupe(items):
     seen = set()
     for item in items:
@@ -13,6 +10,6 @@ if __name__ == '__main__':
     a = [1, 5, 2, 1, 9, 1, 5, 10]
     print(F'a={a}')
     print(f'list(dedupe(a)={list(dedupe(a))}')
-#output:
-#a=[1, 5, 2, 1, 9, 1, 5, 10]
-#list(dedupe(a)=[1, 5, 2, 9, 10]
+
+
+# %%
