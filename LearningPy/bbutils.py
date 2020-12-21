eaf6413a7d29e5fbabe6256ef36425ea3da6d9ac
bbu654 <bbu654@hotmail.com>
2020-12-20 20:59:07 -0800

    Add files via upload

--- LearningPy/bbutils.py
+++ LearningPy/bbutils.py
@@ -448,7 +448,27 @@ def print_format_table():
 			print(s1) 
 		print('\n') 
 
-#print_format_table() 
+#print_format_table()
+# 
+#  
+# dedupeSetEx.py <= example.py
+#
+# Remove duplicate entries from a sequence while keeping order
+#Set
+def dedupe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+#print it thx
+if __name__ == '__main__':
+    a = [1, 5, 2, 1, 9, 1, 5, 10]
+    print(F'a={a}')
+    print(f'list(dedupe(a)={list(dedupe(a))}')
+#output:
+#a=[1, 5, 2, 1, 9, 1, 5, 10]
+#list(dedupe(a)=[1, 5, 2, 9, 10]
 def StringChallenge():
     shorty6=[]
     first_value = '  FIRST challenge         '
